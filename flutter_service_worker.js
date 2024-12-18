aa1860b0bf7eb9fa396caf05d10dc91aa745921a
Osama Nabil <121755701+01121922383osama@users.noreply.github.com>
2024-12-18 19:28:37 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b65cad76e47edd4c79d79f7651271e75
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "2da13cba62d8d92973b06a405f87ecaf",
+"flutter_bootstrap.js": "24ade9135e55fdc228df88b3dad391a3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
